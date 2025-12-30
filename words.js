090586b598ce7fe0d992bba846320a7e591136ff
inficio28 <franck.lefumeux@gmail.com>
2025-12-30 14:03:11 +0100

    Create words.js with random words for the game
    Added a list of 20 random words for the game.

---
+++ words.js
@@ -0,0 +1,24 @@
+// Liste de 20 mots aléatoires pour le jeu
+// Ce fichier doit être chargé avant script.js
+const WORDS = [
+  "aventure",
+  "ordinateur",
+  "fenetre",
+  "montagne",
+  "chocolat",
+  "musique",
+  "jardin",
+  "etoile",
+  "voyage",
+  "bibliotheque",
+  "puzzle",
+  "lumiere",
+  "océan",
+  "horizon",
+  "secret",
+  "cascade",
+  "biscuit",
+  "atelier",
+  "pixel",
+  "sourire"
+];
